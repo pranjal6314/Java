4fa48cd8f72e954e267854999478f91bfc38d9ee
Pranjal Choudhary <77271332+pranjal6314@users.noreply.github.com>
2021-08-26 22:31:27 +0530

    Create demo.java

---
+++ demo.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class MainClass{
+public static void main(String[] args){
+int n,m,c;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+m=sc.nextInt();
+c=n+m;
+System.out.println(n+m+"sum of 2 given number");
+}
+}
